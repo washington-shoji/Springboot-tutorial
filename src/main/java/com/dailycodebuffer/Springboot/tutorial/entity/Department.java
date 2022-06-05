027449a49ce27a0f762f67936f6159d490cde0d4
washington-shoji <shoji.washington@gmail.com>
2022-06-05 16:10:40 +1000

    refactor(lombok): Refactored department to use lombok

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Department.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Department.java
@@ -6,7 +6,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
 
     @Id
@@ -17,53 +26,5 @@ public class Department {
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
-
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-
-    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-
-    public Department() {
-    }
-
-    @Override
-    public String toString() {
-        return "Department [departmentAddress=" + departmentAddress + ", departmentCode=" + departmentCode
-                + ", departmentId=" + departmentId + ", departmentName=" + departmentName + "]";
-    }
 
 }
