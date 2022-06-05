882ec2f6765cfb3b6df47d66cd2a32d3d3bbfcb9
washington-shoji <shoji.washington@gmail.com>
2022-06-05 21:29:28 +1000

    test(repository): Added department repository test

---
+++ src/test/java/com/dailycodebuffer/Springboot/tutorial/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.dailycodebuffer.Springboot.tutorial.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+
+@DataJpaTest
+public class DepartmentRepositoryTest {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        Department department = Department.builder()
+                .departmentName("MK")
+                .departmentAddress("Sydney")
+                .departmentCode("MK-0").build();
+
+        entityManager.persist(department);
+    }
+
+    @Test
+    public void whenFindById_thenReturnDepartment() {
+        Department department = departmentRepository.findById(1L).get();
+
+        assertEquals(department.getDepartmentName(), "MK");
+    }
+}
