b18418787e8b5fac2de101e327b0a0ba293e454c
washington-shoji <shoji.washington@gmail.com>
2022-06-05 15:55:49 +1000

    feat(inerface): Added department interface

---
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Department.java
@@ -0,0 +1,69 @@
+package com.dailycodebuffer.Springboot.tutorial.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+
+    @NotBlank(message = "Please Add Department Name")
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentAddress() {
+        return departmentAddress;
+    }
+
+    public void setDepartmentAddress(String departmentAddress) {
+        this.departmentAddress = departmentAddress;
+    }
+
+    public String getDepartmentCode() {
+        return departmentCode;
+    }
+
+    public void setDepartmentCode(String departmentCode) {
+        this.departmentCode = departmentCode;
+    }
+
+    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.departmentAddress = departmentAddress;
+        this.departmentCode = departmentCode;
+    }
+
+    public Department() {
+    }
+
+    @Override
+    public String toString() {
+        return "Department [departmentAddress=" + departmentAddress + ", departmentCode=" + departmentCode
+                + ", departmentId=" + departmentId + ", departmentName=" + departmentName + "]";
+    }
+
+}
