ba55536b53c9e9353e0f42c7041524d1d987e0f4
washington-shoji <shoji.washington@gmail.com>
2022-06-05 19:20:24 +1000

    refactor(implementation): Refactored the department service implementation, added exception

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentServiceImpl.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentServiceImpl.java
@@ -2,11 +2,13 @@ package com.dailycodebuffer.Springboot.tutorial.service;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+import com.dailycodebuffer.Springboot.tutorial.error.DepartmentNotFoundException;
 import com.dailycodebuffer.Springboot.tutorial.repository.DepartmentRepository;
 
 @Service
@@ -27,8 +29,14 @@ public class DepartmentServiceImpl implements DepartmentService {
     }
 
     @Override
-    public Department fetchDepartmentById(Long departmentId) {
-        return departmentRepository.findById(departmentId).get();
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException {
+        Optional<Department> department = departmentRepository.findById(departmentId);
+
+        if (!department.isPresent()) {
+            throw new DepartmentNotFoundException("Department Not Available!");
+        }
+
+        return department.get();
     }
 
     @Override
